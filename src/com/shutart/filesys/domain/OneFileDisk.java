8a5c510de82d8df823cc80fef4272a168f4c134d
Artem Shutak <artem.shutak@gmail.com>
2012-07-01 20:29:57 +0400

    OneFileDisk.getInstance(fileName, numberOfPages, pageSize) static method was created instead of public constructor.

--- src/com/shutart/filesys/domain/OneFileDisk.java
+++ src/com/shutart/filesys/domain/OneFileDisk.java
@@ -8,34 +8,182 @@ import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class OneFileDisk extends AbstractDisk implements IDisk{
+	
+	private static final Map<String, OneFileDisk> diskName2Inst = new HashMap<String, OneFileDisk>(1);
+	private static final String NUM_OF_PAGES = "numOfPages=";
+	private static final String PAGE_SIZE = "pageSize=";	
+
+	public static boolean exists(String diskName, int numberOfPages,
+			int pageSize) {
+		String fullDiskName = getFullDiskName(diskName, numberOfPages, pageSize);
+		if (diskName2Inst.containsKey(fullDiskName))
+			return true;
+		File diskFile = new File(fullDiskName);
+		return diskFile.exists();
+	}
+	
+	public static OneFileDisk getInstance(String diskName, int numberOfPages,
+			int pageSize) {
+		try {
+			OneFileDisk disk = diskName2Inst.get(diskName);
+			if (disk != null)
+				return disk;
+
+			String fullDiskName = getFullDiskName(diskName, numberOfPages,
+					pageSize);
+			File diskFile = new File(fullDiskName);
+			boolean fileAlreadyExists = diskFile.exists();
+			if (!fileAlreadyExists) {
+				if (!diskFile.createNewFile())
+					throw new IllegalStateException();
+			}
+			
+			OneFileDisk newDiskInst = new OneFileDisk(diskFile, numberOfPages,
+					pageSize);
+			if (fileAlreadyExists) {
+				if (newDiskInst.getSizeInBytes() != diskFile.length())
+					throw new IllegalStateException();
+			} else {
+				newDiskInst.allocateDiskSpace();
+			}
+
+			
+			diskName2Inst.put(fullDiskName, newDiskInst);
+			return newDiskInst;
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	private static String getFullDiskName(String diskName, int numberOfPages,
+			int pageSize) {
+		return diskName + "(" + NUM_OF_PAGES + numberOfPages
+				+ "; " + PAGE_SIZE + pageSize + ")";
+	}
+	
+	
+//	public static OneFileDisk createDisk(String diskName, int numberOfPages,
+//			int pageSize) {
+//		try {
+//			if (diskName2Inst.containsKey(diskName))
+//				return null;
+//
+//			String[] sts = getListOfFilesInThisDirWhichStartWith(diskName);
+//			if (sts.length != 0)
+//				return null;
+//
+//			File realDiskFile = new File(diskName + "(" + NUM_OF_PAGES
+//					+ numberOfPages + "; " + PAGE_SIZE + pageSize + ")");
+//			if (!realDiskFile.createNewFile())
+//				return null;
+//			if (!realDiskFile.isFile())
+//				return null;
+//
+//			OneFileDisk disk = new OneFileDisk(realDiskFile, numberOfPages,
+//					pageSize);
+//			diskName2Inst.put(diskName, disk);
+//			return disk;
+//		} catch (IOException e) {
+//			throw new IllegalStateException(e);
+//		}
+//	}
+//
+//	public static OneFileDisk getDisk(String diskName){
+//		OneFileDisk disk = diskName2Inst.get(diskName);
+//		if (disk != null)
+//			return disk;
+//		
+//		String[] sts = getListOfFilesInThisDirWhichStartWith(diskName);
+//		if (sts.length != 1){
+//			if (sts.length == 0)
+//				return null;
+//			else
+//				throw new IllegalStateException();
+//		}
+//		
+//		String realDiskName = sts[0];
+//		File realFile = new File(realDiskName );
+////		Scanner sc = new Scanner(new ByteArrayInputStream(realDiskName.getBytes()));
+////		if (!sc.hasNextInt())
+////			throw new IllegalStateException();
+//		
+//		int numberOfPages = extractNumberOfPages(realDiskName);
+//		int pageSize = extractPageSize(realDiskName);
+//		return new OneFileDisk(realFile, numberOfPages, pageSize);
+//	}
+
+//	private static int extractPageSize(String realDiskName) {
+//		final int pageSizeStartInd = realDiskName.indexOf(PAGE_SIZE) + PAGE_SIZE.length();
+//		final int pageSizeEndInd = realDiskName.indexOf(")", pageSizeStartInd);
+//		if (pageSizeStartInd == -1 + PAGE_SIZE.length() || 
+//				pageSizeEndInd == -1)
+//			throw new IllegalStateException();
+//		int pageSize = Integer.valueOf(realDiskName.substring(pageSizeStartInd, pageSizeEndInd));
+//		return pageSize;
+//	}
+//
+//	private static int extractNumberOfPages(String realDiskName) {
+//		final int numberOfPagesStartInd = realDiskName.indexOf(NUM_OF_PAGES) + NUM_OF_PAGES.length();
+//		final int numberOfPagesEndInd = realDiskName.indexOf(";", numberOfPagesStartInd);
+//		if (numberOfPagesStartInd == -1 + NUM_OF_PAGES.length() || 
+//				numberOfPagesEndInd == -1)
+//			throw new IllegalStateException();
+//		int numberOfPages = Integer.valueOf(realDiskName.substring(numberOfPagesStartInd, numberOfPagesEndInd));
+//		return numberOfPages;
+//	}
+
+//	private static final String[] NULL_STRING_ARRAY = new String[0];
+//	private static String[] getListOfFilesInThisDirWhichStartWith(String diskName) {
+//		File diskFile = new File(diskName);
+//		if (diskFile.exists())
+//			return NULL_STRING_ARRAY;
+//		String dirName = diskFile.getParent();
+//		final String simpleDiskName = diskFile.getName();
+//		String[] sts = new File(dirName).list(new FilenameFilter() {
+//			@Override
+//			public boolean accept(File dir, String name) {
+//				return name.startsWith(simpleDiskName + "(");
+//			}
+//		});
+//		return sts;
+//	}
+	
+//	public static boolean exist(String diskName){
+//		if(diskName2Inst.containsKey(diskName))
+//			return true;
+//		String dirName = new File(diskName).getPath();
+//	}
 	
 	private final FileChannel channel;
 	private final RandomAccessFile  raf;
 	private final File file;
 
-	public OneFileDisk(String fileName, int numberOfPages, int pageSize) {
+	private OneFileDisk(File file, int numberOfPages, int pageSize) {
 		super(numberOfPages, pageSize);
 		
 		RandomAccessFile  tmpRaf = null;
 		FileChannel tmpChannel = null;
-		file = new File(fileName);
+		this.file = file;
 		try {
-			file.createNewFile();
+			if (!file.exists())
+				throw new IllegalArgumentException();
+//			file.createNewFile();
 			if (numberOfPages*pageSize > file.getFreeSpace())
 				throw new IllegalArgumentException(numberOfPages*pageSize +" > "+ file.getFreeSpace());
 			tmpRaf = new RandomAccessFile(file, "rw");
 			tmpChannel = tmpRaf.getChannel();
 		} catch (FileNotFoundException e) {
 			processCatchCase(tmpRaf, tmpChannel, e);
-		} catch (IOException e) {
-			processCatchCase(tmpRaf, tmpChannel, e);
 		} 
+//		catch (IOException e) {
+//			processCatchCase(tmpRaf, tmpChannel, e);
+//		} 
 		this.channel = tmpChannel;
 		this.raf = tmpRaf;
-		
-		allocateDiskSpace();
 	}
 
 	private void allocateDiskSpace() {
@@ -101,13 +249,29 @@ public final class OneFileDisk extends AbstractDisk implements IDisk{
 
 	@Override
 	public boolean delete() {
-		try {
-			channel.close();
-			raf.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+		boolean fileDeleted = file.delete();
+		if (fileDeleted){
+			try {
+				channel.close();
+				raf.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		return file.delete();
+		return fileDeleted ;
+	}
+	
+	@Override
+	public final boolean equals(Object o){
+		if (! (o instanceof OneFileDisk))
+			return false;
+		OneFileDisk ofd = (OneFileDisk) o;
+		return ofd.file.equals(file);
+	}
+	
+	@Override
+	public final int hashCode(){
+		return file.hashCode();
 	}
 
 }
--- test/com/shutart/onefilefs/test/completed/OneFileDiskTest.java
+++ test/com/shutart/onefilefs/test/completed/OneFileDiskTest.java
@@ -3,12 +3,18 @@ package com.shutart.onefilefs.test.completed;
 import com.shutart.filesys.domain.IDisk;
 import com.shutart.filesys.domain.OneFileDisk;
 import com.shutart.onefilefs.test.util.AbstractDiskTest;
+import static org.junit.Assert.*;
 
-public final class OneFileDiskTest extends AbstractDiskTest{
+public final class OneFileDiskTest extends AbstractDiskTest {
+
+	private static final String DISK_NAME = "testPath/testDiskInOneFile";
 
 	@Override
 	public IDisk getNewDisk(int numberOfPages, int pageSize) {
-		return new OneFileDisk("testPath/testDiskInOneFile", numberOfPages, pageSize);
+		IDisk disk = OneFileDisk
+				.getInstance(DISK_NAME, numberOfPages, pageSize);
+		assertNotNull(disk);
+		return disk;
 	}
 
 }
